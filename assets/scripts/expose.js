8070baf18122aef7df1a7ec61d850639211fa757
longhodac-ucsd <lhodac@ucsd.edu>
2025-05-06 04:08:58 -0700

    update

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,38 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const expose = document.getElementById("expose");
+  const partyHornImg = expose.querySelector('img');
+  const hornSelect = document.getElementById("horn-select");
+  const sound = expose.querySelector('audio.hidden');
+
+  hornSelect.addEventListener('change', () => {
+    const target = hornSelect.value;
+    if(target){ 
+      partyHornImg.src = '/assets/images/' + target + '.svg';
+      partyHornImg.alt = target;
+      sound.src = '/assets/audio/' + target + '.mp3';
+    }else{
+      partyHornImg.src = '/assets/images/no-image.png';
+      partyHornImg.alt = "No image selected";
+      sound.alt = target;
+    }
+    sound.load();
+  });
+
+  const volumeControl = document.getElementById("volume-controls");
+  const slider = volumeControl.querySelector('input');
+  const playButton = expose.querySelector('button');
+
+
+  slider.addEventListener('input', () => {
+    const vol = slider.value / 100;
+    sound.volume = vol;
+  });
+
+  playButton.addEventListener('click', ()=> {
+    sound.play().catch(err => console.warn('Play failed:', err));
+  });
+
+
 }
